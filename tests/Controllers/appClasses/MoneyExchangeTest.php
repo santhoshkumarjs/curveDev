5185aa323200f4d2ecbe070c71beac84544f1ff6
CC-Demo-Testing-at-326986118736 <ec2devops@gmail.com>
2021-01-11 00:18:29 +0530

    Changes

--- tests/Controllers/appClasses/MoneyExchangeTest.php
+++ tests/Controllers/appClasses/MoneyExchangeTest.php
@@ -3,7 +3,12 @@
 class MoneyExchangeTest extends PHPUnit_Framework_TestCase{
 	public function testgetEuroCurrencyValue(){
 		$monyObj = app\Controllers\appClasses\MoneyExchange::getExchangeInstance();
-		$this->assertEquals('0.8163265306',$monyObj->getEuroCurrencyValue('latest'));
+		$this->assertTrue($monyObj->getEuroCurrencyValue('latest') == '0.8163265306');
+	}
+
+	public function testisGoodTimeToExchange(){
+		$monyObj = app\Controllers\appClasses\MoneyExchange::getExchangeInstance();
+		$this->assertTrue($monyObj->isGoodTimeToExchange('0.8163265306','0.8173265306') == false);
 	}
 }
 ?>
